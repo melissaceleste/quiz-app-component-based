397cac85d748daefeb315ba8ad7a1ac6bac90c36
Melissa Celeste Gries <m.c.gries@gmx.de>
2021-02-04 14:22:59 +0100

    add header to storybook

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Button from './components/Button'
 
-const button = Button('Click')
+const button = Button('Click me')
+const button2 = Button('Hold me')
 
 document.body.append(button)
+document.body.append(button2)
